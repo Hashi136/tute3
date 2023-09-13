9208cf050175b5bdb745ed4540ffb9f8cd4c32f6
user <user@DESKTOP-RT943GI>
2023-09-13 13:39:59 +0530

    initial files

---
+++ out/clang-release/src/computer.o.d
@@ -0,0 +1,6 @@
+../out/clang-release/src/computer.o: computer.cc computer.h \
+  C:/omnetpp-6.0.1-windows-x86_64/omnetpp-6.0.1/include/omnetpp.h
+
+computer.h:
+
+C:/omnetpp-6.0.1-windows-x86_64/omnetpp-6.0.1/include/omnetpp.h:
